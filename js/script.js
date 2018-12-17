7bcfd9f93840fa9e68632f9aaa28c2452221d2b5
Spencer Price <spencerjprice@byu.net>
2018-12-16 20:03:47 -0700

    Major updates

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
     $(this).toggleClass("navbar__hamburger--active");
     $(".navbar").toggleClass("navbar--active");
   });
+
+  $('.smooth-goto').on('click', function() {  
+    console.log('yeeet')
+    $('html, body').animate({scrollTop: $(this.hash).offset().top - 50}, 1000);
+    return false;
+});
 });
